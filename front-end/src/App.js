b35d48e8446dc724ae0b942bde868d2a48e84f9e
KhanhVo <vckhanhitiu1@gmail.com>
2018-03-19 23:33:36 +0700

    Implement Homepage interface

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import HomePageComponent from "./components/HomePageComponent";
 
 class App extends Component {
   render() {
     return (
       <div >
         <h1>Hello EnglishForYou</h1>
+        <HomePageComponent/>
       </div>
     );
   }
---
+++ front-end/src/components/HomePageComponent.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class HomePageComponent extends Component{
+
+    render(){
+        return(
+          <div>
+          </div>
+        );
+    }
+}
+
+export default HomePageComponent
