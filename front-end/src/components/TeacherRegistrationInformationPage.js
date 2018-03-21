8cb139e652a9d3c0b45823d0993f5a683fe2f830
KhanhVo <vckhanhitiu1@gmail.com>
2018-03-21 07:30:56 +0700

    Add TeacherRegistrationInformationComponent file

---
+++ front-end/src/components/TeacherRegistrationInformationPage.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+class TeacherRegistrationInformationComponent extends Component {
+    render(){
+        return(
+            <div className = "registration_screen">
+                <div className="registration_form">
+
+                </div>
+            </div>
+        )
+    }
+}
+export default TeacherRegistrationInformationComponent
